67651f35828067df795147f4c845aa07fc834105
G-Baudelaire <germainjones1@gmail.com>
2025-09-17 00:36:48 +0100

    Add Feature Extraction Model Using Placeholder - Modify docker-compose.yml. - Add models.config. - Modify app.py to handle 2 models.

--- application/webapp/app.py
+++ application/webapp/app.py
@@ -7,27 +7,44 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 TF_URL = os.getenv("TF_URL")
+TF_2_URL = os.getenv("TF_2_URL")
 
 app = FastAPI(title="Age & Gender Demo")
 templates = Jinja2Templates(directory="templates")
+
 
 def preprocess(img_bytes):
     img = Image.open(io.BytesIO(img_bytes)).convert("RGB").resize((128, 128))
     x = (np.asarray(img, dtype=np.float32) / 255.0)[None, ...]  # (1,128,128,3)
     return x
 
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     if not file.content_type or not file.content_type.startswith("image/"):
         raise HTTPException(400, "Please upload an image.")
     x = preprocess(await file.read())
+
     try:
-        r = requests.post(TF_URL, json={"instances": x.tolist()}, timeout=10)
-        r.raise_for_status()
+        r1 = requests.post(TF_URL, json={"instances": x.tolist()}, timeout=10)
+        r1.raise_for_status()
     except requests.RequestException as e:
         raise HTTPException(502, f"Model server error: {e}")
-    return r.json()  # keep it simple: return raw TF Serving JSON
+
+    try:
+        r2 = requests.post(TF_2_URL, json={"instances": x.tolist()}, timeout=10)
+        r2.raise_for_status()
+    except requests.RequestException as e:
+        raise HTTPException(502, f"Model server error: {e}")
+
+    combined = {
+        "model_1": r1.json(),
+        "model_2": r2.json()
+    }
+
+    return combined
